04d92e4017b54caba0d88f10716a3c6aed19e03c
Ryoichi Hara <rychhr@gmail.com>
2014-06-10 20:36:04 +0900

    Add HRYLevel

---
+++ CookieCrunch/HRYLevel.h
@@ -0,0 +1,13 @@
+//
+//  HRYLevel.h
+//  CookieCrunch
+//
+//  Created by Ryoichi Hara on 2014/06/10.
+//  Copyright (c) 2014年 Ryoichi Hara. All rights reserved.
+//
+
+@import Foundation;
+
+@interface HRYLevel : NSObject
+
+@end
---
+++ CookieCrunch/HRYLevel.m
@@ -0,0 +1,13 @@
+//
+//  HRYLevel.m
+//  CookieCrunch
+//
+//  Created by Ryoichi Hara on 2014/06/10.
+//  Copyright (c) 2014年 Ryoichi Hara. All rights reserved.
+//
+
+#import "HRYLevel.h"
+
+@implementation HRYLevel
+
+@end
