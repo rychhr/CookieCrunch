5f848457fa923e9d52fa7f1e23198d3a9361d67e
Ryoichi Hara <rychhr@gmail.com>
2014-06-10 16:08:08 +0900

    Add HRYCookie

---
+++ CookieCrunch/HRYCookie.h
@@ -0,0 +1,13 @@
+//
+//  HRYCookie.h
+//  CookieCrunch
+//
+//  Created by Ryoichi Hara on 2014/06/10.
+//  Copyright (c) 2014年 Ryoichi Hara. All rights reserved.
+//
+
+@import Foundation;
+
+@interface HRYCookie : NSObject
+
+@end
---
+++ CookieCrunch/HRYCookie.m
@@ -0,0 +1,13 @@
+//
+//  HRYCookie.m
+//  CookieCrunch
+//
+//  Created by Ryoichi Hara on 2014/06/10.
+//  Copyright (c) 2014年 Ryoichi Hara. All rights reserved.
+//
+
+#import "HRYCookie.h"
+
+@implementation HRYCookie
+
+@end
