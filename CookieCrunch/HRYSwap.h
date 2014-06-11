52ea7918cb59233aa6444fd8aff08e5ce117541e
Ryoichi Hara <rychhr@gmail.com>
2014-06-11 19:19:37 +0900

    Add HRYSwap

---
+++ CookieCrunch/HRYSwap.h
@@ -0,0 +1,13 @@
+//
+//  HRYSwap.h
+//  CookieCrunch
+//
+//  Created by Ryoichi Hara on 2014/06/11.
+//  Copyright (c) 2014年 Ryoichi Hara. All rights reserved.
+//
+
+@import Foundation;
+
+@interface HRYSwap : NSObject
+
+@end
---
+++ CookieCrunch/HRYSwap.m
@@ -0,0 +1,13 @@
+//
+//  HRYSwap.m
+//  CookieCrunch
+//
+//  Created by Ryoichi Hara on 2014/06/11.
+//  Copyright (c) 2014年 Ryoichi Hara. All rights reserved.
+//
+
+#import "HRYSwap.h"
+
+@implementation HRYSwap
+
+@end
