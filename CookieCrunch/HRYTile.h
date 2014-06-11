ad1753bf01454102f94bb3ab20a86ba96392640c
Ryoichi Hara <rychhr@gmail.com>
2014-06-11 14:35:01 +0900

    Add HRYTile

---
+++ CookieCrunch/HRYTile.h
@@ -0,0 +1,14 @@
+//
+//  HRYTile.h
+//  CookieCrunch
+//
+//  Created by Ryoichi Hara on 2014/06/11.
+//  Copyright (c) 2014年 Ryoichi Hara. All rights reserved.
+//
+//  This class is to reprsent a single tile in the 2-D level grid.
+
+@import Foundation;
+
+@interface HRYTile : NSObject
+
+@end
---
+++ CookieCrunch/HRYTile.m
@@ -0,0 +1,13 @@
+//
+//  HRYTile.m
+//  CookieCrunch
+//
+//  Created by Ryoichi Hara on 2014/06/11.
+//  Copyright (c) 2014年 Ryoichi Hara. All rights reserved.
+//
+
+#import "HRYTile.h"
+
+@implementation HRYTile
+
+@end
