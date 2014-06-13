20aa486988982d9e088e830c1fe5a8ded7583f30
Ryoichi Hara <rychhr@gmail.com>
2014-06-13 15:41:32 +0900

    Add HRYChain

---
+++ CookieCrunch/HRYChain.h
@@ -0,0 +1,13 @@
+//
+//  HRYChain.h
+//  CookieCrunch
+//
+//  Created by Ryoichi Hara on 2014/06/13.
+//  Copyright (c) 2014年 Ryoichi Hara. All rights reserved.
+//
+
+@import Foundation;
+
+@interface HRYChain : NSObject
+
+@end
---
+++ CookieCrunch/HRYChain.m
@@ -0,0 +1,13 @@
+//
+//  HRYChain.m
+//  CookieCrunch
+//
+//  Created by Ryoichi Hara on 2014/06/13.
+//  Copyright (c) 2014年 Ryoichi Hara. All rights reserved.
+//
+
+#import "HRYChain.h"
+
+@implementation HRYChain
+
+@end
